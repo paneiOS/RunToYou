8ce6955e56afbbb5beaf25dee9050fcad9551b21
Pane <pane4846@gmail.com>
2024-04-22 21:12:30 +0900

    Initial Commit

---
+++ RunToYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunToYou
+//
+//  Created by 이정환 on 4/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
