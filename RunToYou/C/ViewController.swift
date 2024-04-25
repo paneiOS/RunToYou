91ccf81c31227448a40302f3c9b07aae4cc4bee6
qwe179 <tkdwls0501@naver.com>
2024-04-25 21:12:38 +0900

    - 작업 브랜치 추가 : feat/login - Kakao 로그인 기능 테스트

---
+++ RunToYou/C/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  RunToYou
+//
+//  Created by 이정환 on 4/22/24.
+//
+
+import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+import RxKakaoSDKUser
+import KakaoSDKUser
+
+final class ViewController: UIViewController, StoryboardView {
+    
+    private let loginView = LoginView()
+    var disposeBag = DisposeBag()
+    
+    deinit {
+        print("\(type(of: self)): Deinited")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = loginView
+    }
+    
+    func bind(reactor: ViewReactor) {
+        //Action
+        
+        //increaseButton.rx.tap //rxcocoa
+        loginView.loginButton.rx.tap
+        .map { Reactor.Action.login}
+        .bind(to: reactor.action)
+        .disposed(by: disposeBag)
+        
+        //State
+        reactor.state.map { $0.loginResult }
+            .subscribe(onNext: { result in
+                switch result {
+                case .success(let oauthToken):
+                    print("Login success: \(oauthToken)")
+                    self.kakaoGetUserInfo()
+                    // Handle success
+                case .failure(let error):
+                    print("Login error: \(error)")
+                    // Handle error
+                case .none:
+                    print("nothing")
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func kakaoGetUserInfo() {
+        print("프린트:::")
+        UserApi.shared.rx.me()
+            .subscribe (onSuccess:{ user in
+                print("me() success.")
+
+                //do something
+                let userName = user.kakaoAccount?.name
+                let userEmail = user.kakaoAccount?.email
+                let userGender = user.kakaoAccount?.gender
+                let userProfile = user.kakaoAccount?.profile?.profileImageUrl
+                let userBirthYear = user.kakaoAccount?.birthyear
+
+                let contentText =
+                "user name : \(String(describing: userName))\n userEmail : \(String(describing: userEmail))\n userGender : \(String(describing: userGender)), userBirthYear : \(String(describing: userBirthYear))\n userProfile : \(String(describing: userProfile))"
+
+                print("user - \(user)")
+
+                self.loginView.textField.text = contentText
+
+            }, onFailure: {error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+    }
+   
+    
+
+
+}
