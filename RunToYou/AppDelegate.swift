b06be3e471d16607d66509c8b2811a32ba8b5fb7
qwe179 <tkdwls0501@naver.com>
2024-05-21 17:13:09 +0900

    #9 [FEAT] 구글 로그인을 위한 AppDelegate 에 로그인 api 인스턴스 받아오는 코드 작성

--- RunToYou/AppDelegate.swift
+++ RunToYou/AppDelegate.swift
@@ -9,12 +9,22 @@ import UIKit
 import RxKakaoSDKAuth
 import RxKakaoSDKCommon
 import KakaoSDKAuth
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // kakao login
         RxKakaoSDK.initSDK(appKey: "625a045f6d46a73f5449c3345256774d")
+        // google login
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+          if error != nil || user == nil {
+            // Show the app's signed-out state.
+          } else {
+            // Show the app's signed-in state.
+          }
+        }
         return true
     }
 
@@ -27,6 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: For Kakao API
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [: ]) -> Bool {
+        if GIDSignIn.sharedInstance.handle(url) {
+            return true
+        }
         if AuthApi.isKakaoTalkLoginUrl(url) {
             return AuthController.rx.handleOpenUrl(url: url)
         }
