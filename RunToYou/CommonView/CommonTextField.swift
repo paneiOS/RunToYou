fc7159991cb3c90ffb820f0d82bdd637469a3781
qwe179 <tkdwls0501@naver.com>
2024-05-30 18:33:25 +0900

    #18 [FEAT] 공통으로 많이 사용하는 CommonTextField 개발

---
+++ RunToYou/CommonView/CommonTextField.swift
@@ -0,0 +1,42 @@
+//
+//  CommonTextField.swift
+//  RunToYou
+//
+//  Created by 23 09 on 5/30/24.
+//
+
+import UIKit
+
+final class CommonTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTextField()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTextField()
+    }
+
+    deinit {
+        print("\(type(of: self)): Deinited")
+    }
+
+    private func setupTextField() {
+        backgroundColor = UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1)
+        layer.cornerRadius = 11
+        font = .customFont(.notoSans, family: .regular, size: 16)
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 19, bottom: 0, right: 19))
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 19, bottom: 0, right: 19))
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 19, bottom: 0, right: 19))
+    }
+}
