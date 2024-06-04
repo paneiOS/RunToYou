a86c3b14c79be345b832e31a1b3006a8cbeffd53
qwe179 <tkdwls0501@naver.com>
2024-06-04 12:33:17 +0900

    #20 [FEAT] TeamCreateViewController 생성

---
+++ RunToYou/TeamCreate/TeamCreateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TeamCreateViewController.swift
+//  RunToYou
+//
+//  Created by 23 09 on 6/4/24.
+//
+
+import UIKit
+
+class TeamCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+    }
+}
