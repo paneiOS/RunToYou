97109c1f1817fec89a133ee46a41574c5d5612b5
qwe179 <tkdwls0501@naver.com>
2024-06-13 11:27:46 +0900

    #20 [FEAT] 팀 만들기 화면 뷰 개발

--- RunToYou/TeamCreate/TeamCreateViewController.swift
+++ RunToYou/TeamCreate/TeamCreateViewController.swift
@@ -6,14 +6,102 @@
 //
 
 import UIKit
+import SnapKit
+import ReactorKit
+import RxSwift
+import RxCocoa
 
-class TeamCreateViewController: UIViewController {
+final class TeamCreateViewController: UIViewController {
+    private var buttonConfig: UIButton.Configuration = {
+        var config = UIButton.Configuration.filled()
+        config.imagePadding = 20
+        config.contentInsets = .init(top: 0, leading: 20, bottom: 0, trailing: 0)
+        config.baseBackgroundColor = .clear
+        config.baseForegroundColor = .black
+        return config
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 3
+        label.text = "혹시 초대를 받으셨나요?\n런투유에서는 팀장이 되거나\n기존의 팀에 가입할 수 있어요."
+        label.font = .customFont(.notoSans, family: .regular, size: 20)
+        label.textAlignment = .left
+        return label
+    }()
+    private lazy var teamCreateButton: UIButton = {
+        let btn = UIButton(type: .system)
+        buttonConfig.image = .person
+        buttonConfig.title = "팀 만들기"
+        btn.configuration = buttonConfig
+        btn.layer.cornerRadius = 20
+        btn.contentHorizontalAlignment = .leading
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.2).cgColor
+        return btn
+    }()
+
+    private lazy var teamJoinButton: UIButton = {
+        let btn = UIButton(type: .system)
+        buttonConfig.image = .team
+        buttonConfig.title = "팀 참여하기"
+        btn.configuration = buttonConfig
+        btn.layer.cornerRadius = 20
+        btn.contentHorizontalAlignment = .leading
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.2).cgColor
+        return btn
+    }()
+
+    private let nextButton: CommonButton = {
+        let btn = CommonButton()
+        btn.setupData("다음")
+        btn.makeDisable()
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavi()
         setupLayout()
+        view.backgroundColor = .white
     }
-    
+
     private func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(teamCreateButton)
+        view.addSubview(teamJoinButton)
+        view.addSubview(nextButton)
+        titleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+        }
+        teamCreateButton.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(50)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
+        }
+        teamJoinButton.snp.makeConstraints {
+            $0.top.equalTo(teamCreateButton.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
+        }
+        nextButton.snp.makeConstraints {
+             $0.height.equalTo(55)
+             $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-20)
+         }
+    }
+
+    private func setupNavi() {
+        self.title = "팀만들기"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .white
+        appearance.titleTextAttributes = [.font: UIFont.customFont(.notoSans, family: .medium, size: 20)]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 }
