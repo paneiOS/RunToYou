5345203e7eb12f910842a2eded28c3c4e91876ac
qwe179 <tkdwls0501@naver.com>
2024-06-24 15:38:58 +0900

    #20 [FEAT] 팀만들기 화면 팀만들기,팀참여하기, 다음 버튼 동작 개발

---
+++ RunToYou/TeamCreate/TeamCreateDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TeamCreateDetailViewController.swift
+//  RunToYou
+//
+//  Created by 23 09 on 6/24/24.
+//
+
+import UIKit
+
+class TeamCreateDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RunToYou/TeamCreate/TeamJoinViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TeamJoinViewController.swift
+//  RunToYou
+//
+//  Created by 23 09 on 6/24/24.
+//
+
+import UIKit
+
+class TeamJoinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
